da94bd9ab40eedec48df868acea30e748713de3e
Yuchen Deng <loaden@gmail.com>
2021-10-26 20:54:19 +0800

    压缩包中有校验

--- lfs-03-download-srcs.sh
+++ lfs-03-download-srcs.sh
@@ -7,7 +7,6 @@ mkdir -pv $LFS/sources
 chmod -v a+wt $LFS/sources
 wget http://mirrors.ustc.edu.cn/lfs/lfs-packages/lfs-packages-$(getConf LFS_VERSION).tar --continue --directory-prefix=$LFS/sources
 tar -xf $LFS/sources/lfs-packages-$(getConf LFS_VERSION).tar --directory $LFS/sources
-cp `dirname ${BASH_SOURCE[0]}`/md5sums $LFS/sources/$(getConf LFS_VERSION)/md5sums
 pushd $LFS/sources/$(getConf LFS_VERSION)
     md5sum -c md5sums
 popd
