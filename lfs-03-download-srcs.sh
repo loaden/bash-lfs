4c585e6c78687d01ac205354b95f66e51cdd5efa
Yuchen Deng <loaden@gmail.com>
2021-10-26 20:50:36 +0800

    完成下载

--- lfs-03-download-srcs.sh
+++ lfs-03-download-srcs.sh
@@ -5,8 +5,9 @@ source `dirname ${BASH_SOURCE[0]}`/lfs.sh
 
 mkdir -pv $LFS/sources
 chmod -v a+wt $LFS/sources
-wget --input-file=`dirname ${BASH_SOURCE[0]}`/wget-list --continue --directory-prefix=$LFS/sources
-cp `dirname ${BASH_SOURCE[0]}`/md5sums $LFS/sources/md5sums
-pushd $LFS/sources
+wget http://mirrors.ustc.edu.cn/lfs/lfs-packages/lfs-packages-$(getConf LFS_VERSION).tar --continue --directory-prefix=$LFS/sources
+tar -xf $LFS/sources/lfs-packages-$(getConf LFS_VERSION).tar --directory $LFS/sources
+cp `dirname ${BASH_SOURCE[0]}`/md5sums $LFS/sources/$(getConf LFS_VERSION)/md5sums
+pushd $LFS/sources/$(getConf LFS_VERSION)
     md5sum -c md5sums
 popd
