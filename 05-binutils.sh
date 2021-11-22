17dddaf49e2c438d1cf4e956cba35604f791f809
Yuchen Deng <loaden@gmail.com>
2021-11-22 11:30:14 +0800

    添加glibc编译

--- 05-binutils.sh
+++ 05-binutils.sh
@@ -7,8 +7,8 @@ pushd $LFS/sources/$(getConf LFS_VERSION)
     [ "$CLEAN" ] && rm -rf $(find . -maxdepth 1 -type d -name "binutils-*")
     tar --keep-newer-files -xf $(find . -maxdepth 1 -type f -name binutils-*.tar.*) 2>/dev/null
     cd $(find . -maxdepth 1 -type d -name "binutils-*")
-    [ -f PATCHED ] && patch -p1 -R < ../binutils-2.37-upstream_fix-1.patch
-    patch -p1 < ../binutils-2.37-upstream_fix-1.patch
+    [ -f PATCHED ] && patch -p1 -R < $(find .. -maxdepth 1 -type f -name binutils-*.patch)
+    patch -p1 < $(find .. -maxdepth 1 -type f -name binutils-*.patch)
     touch PATCHED
     mkdir -v build
     cd build
---
+++ 08-glibc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 维护：Yuchen Deng QQ群：19346666、111601117
+
+source `dirname ${BASH_SOURCE[0]}`/lfs.sh
+
+pushd $LFS/sources/$(getConf LFS_VERSION)
+    [ "$CLEAN" ] && rm -rf $(find . -maxdepth 1 -type d -name "glibc-*")
+    tar --keep-newer-files -xf $(find . -maxdepth 1 -type f -name glibc-*.tar.*) 2>/dev/null
+    cd $(find . -maxdepth 1 -type d -name "glibc-*")
+    [ -f PATCHED ] && patch -p1 -R < $(find .. -maxdepth 1 -type f -name glibc-*.patch)
+    patch -p1 < $(find .. -maxdepth 1 -type f -name glibc-*.patch)
+    touch PATCHED
+
+    mkdir -v build
+    cd build
+
+    case $(uname -m) in
+        i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+        ;;
+        x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+                ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+        ;;
+    esac
+
+    echo "rootsbindir=/usr/sbin" > configparms
+
+    ../configure                            \
+        --prefix=/usr                       \
+        --host=$LFS_TGT                     \
+        --build=$(../scripts/config.guess)  \
+        --enable-kernel=3.2                 \
+        --with-headers=$LFS/usr/include     \
+        libc_cv_slibdir=/usr/lib
+    make -j 1
+    make DESTDIR=$LFS install
+popd
