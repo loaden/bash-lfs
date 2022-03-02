f03ec2f6772ea536c6d7ae3e6bc99658aae395b2
Yuchen Deng <loaden@gmail.com>
2022-03-02 13:43:32 +0800

    更新编译选项

--- 02-mount.sh
+++ 02-mount.sh
@@ -3,7 +3,7 @@
 
 source `dirname ${BASH_SOURCE[0]}`/lfs.sh
 
-[ -d $LFS ] || mkdir -pv $LFS
+[ -d $LFS ] || mkdir -pv $LFS && chmod 755 $LFS
 if ! mountpoint -q $LFS; then
     mount -v $(getConf LFS_ROOT_PARTITION) $LFS
     grep $LFS /proc/mounts
