d25653f0cad7dbf0d68ad12ab0f8747a139f9a6c
Yuchen Deng <loaden@gmail.com>
2022-03-03 19:08:51 +0800

    乌克兰加油！

---
+++ 20-make.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# QQ群：111601117、钉钉群：35948877
+
+source `dirname ${BASH_SOURCE[0]}`/lfs.sh
+
+# 配置LFS用户编译任务
+if [ "$USER" != "lfs" ]; then
+    echo "$LFS_PROJECT/`basename ${BASH_SOURCE[0]}`" > /home/lfs/build.sh
+    chown lfs:lfs /home/lfs/build.sh
+    su - lfs
+    return
+fi
+
+# 来自lfs用户的调用
+pushd $LFS/sources/$(getConf LFS_VERSION)
+    PKG_NAME=make
+    PKG_PATH=$(find . -maxdepth 1 -type d -name "$PKG_NAME-*")
+    if [ -z $PKG_PATH ]; then
+        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.*")
+        PKG_PATH=$(find . -maxdepth 1 -type d -name "$PKG_NAME-*")
+    fi
+
+    if [ ! -f $PKG_PATH/_BUILD_DONE ]; then
+        pushd $PKG_PATH
+            ./configure --prefix=/usr   \
+                --without-guile         \
+                --host=$LFS_TGT         \
+                --build=$(build-aux/config.guess)
+            make -j$LFS_BUILD_PROC && make DESTDIR=$LFS install
+            if [ $? = 0 ]; then
+                touch _BUILD_DONE
+            else
+                exit 1
+            fi
+        popd
+    fi
+popd
--- stage2/step1.sh
+++ stage2/step1.sh
@@ -24,3 +24,58 @@ echo -e "\033[31mKILL 13-coreutils.sh ...\033[0m"
 source `dirname ${BASH_SOURCE[0]}`/../13-coreutils.sh
 echo DONE
 echo
+
+echo -e "\033[31mKILL 14-diffutils.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../14-diffutils.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 15-file.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../15-file.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 16-findutils.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../16-findutils.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 17-gawk.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../17-gawk.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 18-grep.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../18-grep.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 19-gzip.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../19-gzip.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 20-make.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../20-make.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 21-patch.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../21-patch.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 22-sed.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../22-sed.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 23-tar.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../23-tar.sh
+echo DONE
+echo
+
+echo -e "\033[31mKILL 24-xz.sh ...\033[0m"
+source `dirname ${BASH_SOURCE[0]}`/../24-xz.sh
+echo DONE
+echo
