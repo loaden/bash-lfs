c7fa008757bc1e550a33ede6236b693552b1a74a
Yuchen Deng <loaden@gmail.com>
2022-03-06 21:39:30 +0800

    整体运行添加错误判断

--- all.sh
+++ all.sh
@@ -3,12 +3,18 @@
 
 # stage1
 sudo `dirname ${BASH_SOURCE[0]}`/stage1/step1.sh
+[ $? = 0 ] || exit 3
 sudo `dirname ${BASH_SOURCE[0]}`/stage1/step2.sh
+[ $? = 0 ] || exit 3
 
 # stage2
 sudo `dirname ${BASH_SOURCE[0]}`/stage2/step1.sh
+[ $? = 0 ] || exit 3
 sudo `dirname ${BASH_SOURCE[0]}`/stage2/step2.sh
+[ $? = 0 ] || exit 3
 sudo `dirname ${BASH_SOURCE[0]}`/stage2/step3.sh
+[ $? = 0 ] || exit 3
 
 # stage3
 sudo `dirname ${BASH_SOURCE[0]}`/stage3/step1.sh
+[ $? = 0 ] || exit 3
