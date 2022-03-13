1f243f22403f2e3f89fc1cee1cf1552138519f85
Yuchen Deng <loaden@gmail.com>
2022-03-13 17:17:11 +0800

    修复路径错误

--- stage3/step4.sh
+++ stage3/step4.sh
@@ -6,16 +6,20 @@
 #
 
 if [ ! -f $LFS/task.sh ]; then
-    source `dirname ${BASH_SOURCE[0]}`/lfs.sh
+    source `dirname ${BASH_SOURCE[0]}`/../lfs.sh
     cp -v ${BASH_SOURCE[0]} $LFS/task.sh
     sed "s/_LFS_VERSION/$(getConf LFS_VERSION)/g" -i $LFS/task.sh
     sed "s/_LFS_BUILD_PROC/$LFS_BUILD_PROC/g" -i $LFS/task.sh
-    source `dirname ${BASH_SOURCE[0]}`/chroot.sh
+    source `dirname ${BASH_SOURCE[0]}`/../chroot.sh
     rm -fv $LFS/task.sh
     exit
 fi
 
 # 来自chroot之后的调用
+
+bootctl
+exit 666
+
 save_usrlib="$(cd /usr/lib; ls ld-linux*)
              libc.so.6
              libthread_db.so.1
