4438587fc649d1c080791ac1cbd279c9dab89f98
Yuchen Deng <loaden@gmail.com>
2022-03-13 12:33:44 +0800

    添加清理与配置

--- stage3/step3.sh
+++ stage3/step3.sh
@@ -5,6 +5,17 @@
 # stage3重新运行失败的测试，看看有没有奇迹发生
 #
 
+if [ ! -f $LFS/task.sh ]; then
+    source `dirname ${BASH_SOURCE[0]}`/lfs.sh
+    cp -v ${BASH_SOURCE[0]} $LFS/task.sh
+    sed "s/_LFS_VERSION/$(getConf LFS_VERSION)/g" -i $LFS/task.sh
+    sed "s/_LFS_BUILD_PROC/$LFS_BUILD_PROC/g" -i $LFS/task.sh
+    source `dirname ${BASH_SOURCE[0]}`/chroot.sh
+    rm -fv $LFS/task.sh
+    exit
+fi
+
+# 来自chroot之后的调用
 pushd /sources/_LFS_VERSION
     PKG_NAME=libtool
     PKG_PATH=$(find . -maxdepth 1 -type d -name "$PKG_NAME-*")
