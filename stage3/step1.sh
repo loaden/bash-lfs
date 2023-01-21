4573e8db0f3f8f2a8b6be84305c4a6f6565067f5
Yuchen Deng <loaden@gmail.com>
2023-01-22 00:53:43 +0800

    删除不需要的编译环节

--- stage3/step1.sh
+++ stage3/step1.sh
@@ -11,12 +11,6 @@ bash `dirname ${BASH_SOURCE[0]}`/../27-init-chroot.sh
 echo DONE
 echo
 
-echo -e "\033[31mKILL 28-libstdcxx.sh ...\033[0m"
-bash `dirname ${BASH_SOURCE[0]}`/../28-libstdcxx.sh
-[ $? = 0 ] || exit 2
-echo DONE
-echo
-
 echo -e "\033[31mKILL 29-gettext-bison.sh ...\033[0m"
 bash `dirname ${BASH_SOURCE[0]}`/../29-gettext-bison.sh
 [ $? = 0 ] || exit 2
