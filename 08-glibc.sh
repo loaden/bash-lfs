4c3fdf5969c2220eeed6a933be95d4fa3f32ff41
Yuchen Deng <loaden@gmail.com>
2023-01-21 12:21:01 +0800

    完成glibc升级

--- 08-glibc.sh
+++ 08-glibc.sh
@@ -46,25 +46,31 @@ pushd $LFS/sources/$(getConf LFS_VERSION)
 
             # 确保将 ldconfig 和 sln 工具安装到 /usr/sbin 目录中
             echo "rootsbindir=/usr/sbin" > configparms
-            ../configure                            \
-                --prefix=/usr                       \
-                --host=$LFS_TGT                     \
-                --build=$(../scripts/config.guess)  \
-                --enable-kernel=3.2                 \
-                --with-headers=$LFS/usr/include     \
+            ../configure                             \
+                --prefix=/usr                      \
+                --host=$LFS_TGT                    \
+                --build=$(../scripts/config.guess) \
+                --enable-kernel=3.2                \
+                --with-headers=$LFS/usr/include    \
                 libc_cv_slibdir=/usr/lib
 
+            # 必须单线程编译
             make -j1 && make DESTDIR=$LFS install
 
-            # 注意看这里的输出，必须类似：[Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]
             if [ $? = 0 ]; then
+                # 改正 ldd 脚本中硬编码的可执行文件加载器路径
                 sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
-                echo 'int main(){}' > dummy.c
-                $LFS_TGT-gcc dummy.c
-                readelf -l a.out | grep '/ld-linux'
-                [ $? != 0 ] && exit 1
-                rm -v dummy.c a.out
-                $LFS/tools/libexec/gcc/$LFS_TGT/$(getConf LFS_GCC_VERSION)/install-tools/mkheaders
+                # 检查，应该输出
+                # [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]
+                # 32 位机器，解释器的名字将会是 /lib/ld-linux.so.2
+                # 还要检查gcc的位置是否来自$LFS
+                whereis $LFS_TGT-gcc
+                echo 'int main(){}' | $LFS_TGT-gcc -xc -
+                readelf -l a.out | grep ld-linux
+                rm -v a.out
+                sleep 5
+                # 完成 limits.h 头文件的安装
+                $LFS/tools/libexec/gcc/$LFS_TGT/12.2.0/install-tools/mkheaders
                 touch _BUILD_DONE
             else
                 pwd
