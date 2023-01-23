0ac1bbac84d5e2f5248716215eb1eb4d27bdc50e
Yuchen Deng <loaden@gmail.com>
2023-01-23 11:24:29 +0800

    完成gcc升级

--- 39-gcc.sh
+++ 39-gcc.sh
@@ -18,6 +18,8 @@ pushd /sources/_LFS_VERSION
     PKG_NAME=gcc
     PKG_PATH=$(find stage4 -maxdepth 1 -type d -name "$PKG_NAME-*")
     if [ -z $PKG_PATH ]; then
+        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.*") --directory stage4
+        PKG_PATH=$(find stage4 -maxdepth 1 -type d -name "$PKG_NAME-*")
         pushd $PKG_PATH
             tar -xpvf $(find ../.. -maxdepth 1 -type f -name mpfr-*.tar.*)
             mv -v $(find . -maxdepth 1 -type d -name "mpfr-*") mpfr
@@ -25,19 +27,17 @@ pushd /sources/_LFS_VERSION
             mv -v $(find . -maxdepth 1 -type d -name "gmp-*") gmp
             tar -xpvf $(find ../.. -maxdepth 1 -type f -name mpc-*.tar.*)
             mv -v $(find . -maxdepth 1 -type d -name "mpc-*") mpc
+
+            # 在 x86_64 上构建时，修改存放 64 位库的默认路径为 “lib”:
             case $(uname -m) in
                 x86_64)
-                    sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+                    sed -e '/m64=/s/lib64/lib/' \
+                        -i.orig gcc/config/i386/t-linux64
                 ;;
             esac
-            cp -v libgcc/Makefile.in libgcc/Makefile.in.bak
-            cp -v libstdc++-v3/include/Makefile.in libstdc++-v3/include/Makefile.in.bak
-            sed '/thread_header =/s/@.*@/gthr-posix.h/' \
-                -i libgcc/Makefile.in libstdc++-v3/include/Makefile.in
+
             echo "---确认---"
             diff gcc/config/i386/t-linux64.orig gcc/config/i386/t-linux64
-            diff libstdc++-v3/include/Makefile.in.bak libstdc++-v3/include/Makefile.in
-            diff libstdc++-v3/include/Makefile.in.bak libstdc++-v3/include/Makefile.in
             echo "------"
             sleep 5
         popd
@@ -46,17 +46,6 @@ pushd /sources/_LFS_VERSION
     if [ ! -f $PKG_PATH/build/_BUILD_DONE ]; then
         mkdir -pv $PKG_PATH/build
         pushd $PKG_PATH
-            rm -rfv mpfr mpc gmp
-            sed -e '/static.*SIGSTKSZ/d' \
-                -e 's/return kAltStackSize/return SIGSTKSZ * 4/' \
-                -i libsanitizer/sanitizer_common/sanitizer_posix_libcdep.cpp
-            case $(uname -m) in
-                x86_64)
-                    sed -e '/m64=/s/lib64/lib/' \
-                        -i.orig gcc/config/i386/t-linux64
-                ;;
-            esac
-
             cd build
             ../configure --prefix=/usr   \
                 LD=ld                    \
@@ -65,36 +54,82 @@ pushd /sources/_LFS_VERSION
                 --disable-bootstrap      \
                 --with-system-zlib
 
-            make -j_LFS_BUILD_PROC || exit 99
-            ulimit -s 32768
-            chown -Rv tester .
-            su tester -c "PATH=$PATH make -j_LFS_BUILD_PROC -k check"
-            ../contrib/test_summary | grep -A7 Summ
-            read -p "查看GCC测试结果，任意键继续..."
-            make install
-            rm -rf /usr/lib/gcc/$(gcc -dumpmachine)/11.2.0/include-fixed/bits/
+            [ $? = 0 ] && make -j_LFS_BUILD_PROC
+
             if [ $? = 0 ]; then
+                # 运行测试前要增加栈空间
+                ulimit -s 32768
+                # 以非特权用户身份测试编译结果，但出错时继续执行其他测试
+                chown -Rv tester .
+                su tester -c "PATH=$PATH make -k check"
+                # 查看测试结果
+                # 对比：https://www.linuxfromscratch.org/lfs/build-logs/
+                ../contrib/test_summary | grep -A7 Summ
+                read -p "$PKG_NAME CHECK DONE..."
+            fi
+
+            [ $? = 0 ] && make install
+
+            if [ $? = 0 ]; then
+                # 修正安装的头文件目录 (及其内容) 不正确的所有权
                 chown -v -R root:root \
-                    /usr/lib/gcc/*linux-gnu/11.2.0/include{,-fixed}
+                    /usr/lib/gcc/$(gcc -dumpmachine)/12.2.0/include{,-fixed}
+                # 创建一个 FHS 因 “历史原因” 要求的符号链接
                 ln -svr /usr/bin/cpp /usr/lib
-                ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/11.2.0/liblto_plugin.so \
+                # 创建一个兼容性符号链接，以支持在构建程序时使用链接时优化 (LTO)
+                ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/12.2.0/liblto_plugin.so \
                     /usr/lib/bfd-plugins/
+
+                # 确认编译和链接像我们期望的一样正常工作
                 echo 'int main(){}' > dummy.c
                 cc dummy.c -v -Wl,--verbose &> dummy.log
+                # 输出 [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]
+                echo ------
                 readelf -l a.out | grep ': /lib'
+                # 确认我们的设定能够使用正确的启动文件
+                # 输出
+                # /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/../../../../lib/crt1.o succeeded
+                # /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/../../../../lib/crti.o succeeded
+                # /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/../../../../lib/crtn.o succeeded
+                echo ------
                 grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
-                read -p "gcc 应该找到所有三个 crt*.o 文件，它们应该位于 /usr/lib 目录中，任意键继续..."
-
+                # 确认编译器能正确查找头文件
+                # 输出
+                # /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include
+                # /usr/local/include
+                # /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include-fixed
+                # /usr/include
+                echo ------
                 grep -B4 '^ /usr/include' dummy.log
+                # 确认新的链接器使用了正确的搜索路径
+                # 输出
+                # SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib64")
+                # SEARCH_DIR("/usr/local/lib64")
+                # SEARCH_DIR("/lib64")
+                # SEARCH_DIR("/usr/lib64")
+                # SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib")
+                # SEARCH_DIR("/usr/local/lib")
+                # SEARCH_DIR("/lib")
+                # SEARCH_DIR("/usr/lib");
+                echo ------
                 grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
+                # 确认我们使用了正确的 libc
+                # 输出 attempt to open /usr/lib/libc.so.6 succeeded
+                echo ------
                 grep "/lib.*/libc.so.6 " dummy.log
+                # 确认 GCC 使用了正确的动态链接器
+                # 输出 found ld-linux-x86-64.so.2 at /usr/lib/ld-linux-x86-64.so.2
+                echo ------
                 grep found dummy.log
-                read -p "耐心的检查日志..."
-
+                # 删除测试文件
+                read -p "这里出现的任何问题在继续构建前都必须解决，耐心的检查日志..."
                 rm -v dummy.c a.out dummy.log
+
+                # 移动一个位置不正确的文件
                 mkdir -pv /usr/share/gdb/auto-load/usr/lib
                 mv -v /usr/lib/*gdb.py /usr/share/gdb/auto-load/usr/lib
 
+                read -p "$PKG_NAME ALL DONE..."
                 touch _BUILD_DONE
             else
                 pwd
