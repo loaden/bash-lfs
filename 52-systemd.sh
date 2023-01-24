4c8ddf20220b7ae40f65ffc0b0433f3ed93c7dc1
Yuchen Deng <loaden@gmail.com>
2023-01-24 22:17:52 +0800

    升级systemd

--- 52-systemd.sh
+++ 52-systemd.sh
@@ -18,7 +18,7 @@ pushd /sources/_LFS_VERSION
     PKG_NAME=systemd
     PKG_PATH=$(find stage4 -maxdepth 1 -type d -name "$PKG_NAME-*")
     if [ -z $PKG_PATH ]; then
-        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.gz")
+        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-???.tar.gz") --directory stage4
         PKG_PATH=$(find stage4 -maxdepth 1 -type d -name "$PKG_NAME-*")
         pushd $PKG_PATH
             find ../.. -maxdepth 1 -type f -name "$PKG_NAME-*.patch" -exec patch -Np1 -i {} \;
@@ -28,36 +28,42 @@ pushd /sources/_LFS_VERSION
 
     if [ ! -f $PKG_PATH/build/_BUILD_DONE ]; then
         pushd $PKG_PATH
+            # 从默认的 udev 规则中删除不必要的组 render 和 sgx
             sed -i -e 's/GROUP="render"/GROUP="video"/' \
                    -e 's/GROUP="sgx", //' rules.d/50-udev-default.rules.in
+
             mkdir -p build
             cd       build
 
             meson --prefix=/usr               \
-                --sysconfdir=/etc             \
-                --localstatedir=/var          \
                 --buildtype=release           \
-                -Dblkid=true                  \
                 -Ddefault-dnssec=no           \
                 -Dfirstboot=false             \
                 -Dinstall-tests=false         \
                 -Dldconfig=false              \
                 -Dsysusers=false              \
-                -Db_lto=false                 \
                 -Drpmmacrosdir=no             \
                 -Dhomed=false                 \
                 -Duserdb=false                \
                 -Dman=false                   \
                 -Dmode=release                \
-                -Ddocdir=/usr/share/doc/systemd-250 \
+                -Dpamconfdir=no               \
+                -Ddocdir=/usr/share/doc/systemd-251 \
                 ..
 
-            ninja -j_LFS_BUILD_PROC && ninja install
+            ninja && ninja install
+
             if [ $? = 0 ]; then
-                tar -xpvf $(find ../.. -maxdepth 1 -type f -name "$PKG_NAME-man-*.tar.*") --strip-components=1 -C /usr/share/man
-                rm -rf /usr/lib/pam.d
+                # 安装 man 页面
+                tar -xpvf $(find /sources/_LFS_VERSION -maxdepth 1 -type f -name "$PKG_NAME-man-*.tar.*") --strip-components=1 -C /usr/share/man
+                # 创建 /etc/machine-id 文件
                 systemd-machine-id-setup
+                # 设定启动目标单元的基本结构
                 systemctl preset-all
+                # 禁用用于升级二进制发行版的服务
+                systemctl disable systemd-sysupdate
+
+                read -p "$PKG_NAME ALL DONE..."
                 touch _BUILD_DONE
             else
                 pwd
