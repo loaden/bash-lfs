4888d10850b922de2c545fd0f9ff969600c661dc
Yuchen Deng <loaden@gmail.com>
2023-01-25 20:40:37 +0800

    移除不必要依赖

--- 63-grub-final.sh
+++ 63-grub-final.sh
@@ -54,26 +54,6 @@ if [ $? = 0 ]; then
             PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
             sleep 1
         done
-
-        # grub 依赖：https://linuxfromscratch.org/blfs/view/stable-systemd/postlfs/grub-efi.html
-        PKG_NAME=unifont
-        PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
-        while [ -z $PKG_PATH ]
-        do
-            wget https://unifoundry.com/pub/unifont/unifont-14.0.04/font-builds/unifont-14.0.04.pcf.gz
-            PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
-            sleep 1
-        done
-
-        # grub 依赖：https://linuxfromscratch.org/blfs/view/stable-systemd/postlfs/dosfstools.html
-        PKG_NAME=dosfstools
-        PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
-        while [ -z $PKG_PATH ]
-        do
-            wget https://mirrors.aliyun.com/blfs/11.2/d/dosfstools-4.2.tar.gz
-            PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
-            sleep 1
-        done
     popd
 
     # 准备chroot
@@ -196,34 +176,6 @@ pushd /sources/_LFS_VERSION
 popd
 
 pushd /sources/_LFS_VERSION
-    PKG_NAME=dosfstools
-    PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
-    if [ -z $PKG_PATH ]; then
-        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.*") --directory stage5
-        PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
-    fi
-
-    if [ ! -f $PKG_PATH/_BUILD_DONE ]; then
-        pushd $PKG_PATH
-            ./configure --prefix=/usr    \
-                --enable-compat-symlinks \
-                --mandir=/usr/share/man  \
-                --docdir=/usr/share/doc/dosfstools-4.2 &&
-            make
-
-            [ $? = 0 ] && make install
-            if [ $? = 0 ]; then
-                read -p "$PKG_NAME ALL DONE..."
-                touch _BUILD_DONE
-            else
-                pwd
-                exit 1
-            fi
-        popd
-    fi
-popd
-
-pushd /sources/_LFS_VERSION
     PKG_NAME=grub
     PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
     if [ -z $PKG_PATH ]; then
@@ -233,9 +185,6 @@ pushd /sources/_LFS_VERSION
 
     if [ ! -f $PKG_PATH/_BUILD_DONE ]; then
         pushd $PKG_PATH
-            mkdir -pv /usr/share/fonts/unifont &&
-            gunzip -c /sources/_LFS_VERSION/unifont-14.0.04.pcf.gz > /usr/share/fonts/unifont/unifont.pcf
-
             unset {C,CPP,CXX,LD}FLAGS
 
             ./configure --prefix=/usr        \
