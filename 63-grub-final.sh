a80708c660076ccde17a5ab4d5bea1378edeeb98
Yuchen Deng <loaden@gmail.com>
2023-01-26 11:07:21 +0800

    尝试dracut

--- 63-grub-final.sh
+++ 63-grub-final.sh
@@ -64,6 +64,37 @@ if [ $? = 0 ]; then
             PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
             sleep 1
         done
+
+        # dracut 生成 initramfs：https://github.com/dracutdevs/dracut
+        PKG_NAME=dracut
+        PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
+        while [ -z $PKG_PATH ]
+        do
+            wget https://github.com/dracutdevs/dracut/archive/refs/tags/059.tar.gz
+            [ -f 059.tar.gz ] && mv -vf 059.tar.gz dracut-059.tar.gz
+            PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
+            sleep 1
+        done
+
+        # btrfs-progs：https://linuxfromscratch.org/blfs/view/stable-systemd/postlfs/btrfs-progs.html
+        PKG_NAME=btrfs-progs
+        PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
+        while [ -z $PKG_PATH ]
+        do
+            wget https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v5.19.tar.xz
+            PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
+            sleep 1
+        done
+
+        # btrfs-progs 依赖：https://linuxfromscratch.org/blfs/view/stable-systemd/general/lzo.html
+        PKG_NAME=lzo
+        PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
+        while [ -z $PKG_PATH ]
+        do
+            wget https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz
+            PKG_PATH=$(find . -maxdepth 1 -type f -name "$PKG_NAME-*.*")
+            sleep 1
+        done
     popd
 
     # 准备chroot
@@ -258,6 +289,99 @@ pushd /sources/_LFS_VERSION
     fi
 popd
 
+pushd /sources/_LFS_VERSION
+    PKG_NAME=lzo
+    PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
+    if [ -z $PKG_PATH ]; then
+        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.*") --directory stage5
+        PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
+    fi
+
+    if [ ! -f $PKG_PATH/_BUILD_DONE ]; then
+        pushd $PKG_PATH
+            ./configure --prefix=/usr            \
+                --enable-shared                  \
+                --disable-static                 \
+                --docdir=/usr/share/doc/lzo-2.10
+
+            [ $? = 0 ] && make
+            [ $? = 0 ] && make check && read -p "$PKG_NAME CHECK DONE..."
+            [ $? = 0 ] && make test && read -p "$PKG_NAME TEST DONE..."
+            [ $? = 0 ] && make install
+            if [ $? = 0 ]; then
+                read -p "$PKG_NAME ALL DONE..."
+                touch _BUILD_DONE
+            else
+                pwd
+                exit 1
+            fi
+        popd
+    fi
+popd
+
+pushd /sources/_LFS_VERSION
+    PKG_NAME=btrfs-progs
+    PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
+    if [ -z $PKG_PATH ]; then
+        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.*") --directory stage5
+        PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
+    fi
+
+    if [ ! -f $PKG_PATH/_BUILD_DONE ]; then
+        pushd $PKG_PATH
+            ./configure --prefix=/usr --disable-documentation
+
+            [ $? = 0 ] && make
+            [ $? = 0 ] && make install
+            if [ $? = 0 ]; then
+                read -p "$PKG_NAME ALL DONE..."
+                touch _BUILD_DONE
+            else
+                pwd
+                exit 1
+            fi
+        popd
+    fi
+popd
+
+pushd /sources/_LFS_VERSION
+    PKG_NAME=dracut
+    PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
+    if [ -z $PKG_PATH ]; then
+        tar -xpvf $(find . -maxdepth 1 -type f -name "$PKG_NAME-*.tar.*") --directory stage5
+        PKG_PATH=$(find stage5 -maxdepth 1 -type d -name "$PKG_NAME-*")
+    fi
+
+    if [ ! -f $PKG_PATH/_BUILD_DONE ]; then
+        pushd $PKG_PATH
+            ./configure             \
+                --sysconfdir=/etc   \
+                --sbindir=/sbin     \
+                --disable-documentation
+
+            [ $? = 0 ] && make && make fssum
+            if [ $? = 0 ]; then
+                pushd tests
+                    ./fsck-tests.sh
+                    ./mkfs-tests.sh
+                    ./cli-tests.sh
+                    ./convert-tests.sh
+                    ./misc-tests.sh
+                    ./fuzz-tests.sh
+                    read -p "$PKG_NAME TEST DONE..."
+                popd
+            fi
+            [ $? = 0 ] && make install
+            if [ $? = 0 ]; then
+                read -p "$PKG_NAME ALL DONE..."
+                touch _BUILD_DONE
+            else
+                pwd
+                exit 1
+            fi
+        popd
+    fi
+popd
 
 # initramfs 生成脚本
 cat > /usr/sbin/mkinitramfs << "EOF"
@@ -624,10 +748,18 @@ exec switch_root /.root "$init" "$@"
 EOF
 
 
-# 生成 initramfs
-mkinitramfs 5.19.2
-mv initrd.img-* /boot/initramfs.img
-ls -lh /boot
+# 脚本生成 initramfs
+# 不支持 btrfs 子卷
+# mkinitramfs 5.19.2
+# mv initrd.img-* /boot/initramfs.img
+# ls -lh /boot
+
+# dracut 生成 initramfs
+dracut /boot/initramfs.img \
+    --force --no-kernel --hostonly --modules "rootfs-block base btrfs" \
+    --early-microcode --fstab --zstd
+
+exit
 
 # GRUB 安装与配置
 
